e9a4d5f169fa1e2a79034c6c4e2eb0577dcd9578
Metehan Cekic <metehancekic@umail.ucsb.edu>
2021-11-15 12:42:49 -0800

    added correlations plotting py

--- src/sh/corr_mnist.sh
+++ src/sh/corr_mnist.sh
@@ -3,15 +3,11 @@
 export CUDA_VISIBLE_DEVICES="0"
 export PYTHONPATH="/home/metehan/hebbian/src/lib/"
 
-declare -a arr=(0.00001)
 
-for i in "${arr[@]}"
-do
-	COMMAND="python -m src.lib.mpl_utils.settings.py"
-	echo $COMMAND
-	eval $COMMAND
-	COMMAND="python -m src.correlations_mnist"
-	echo $COMMAND
-	eval $COMMAND
-done
+COMMAND="python -m src.correlations_mnist"
+echo $COMMAND
+eval $COMMAND
+
+
+
 
